86fef0d6578aec4c6f458ca7168fdcc9e49f5509
Pedro Del Gallego <pedro.delgallego@gmail.com>
2015-03-20 13:03:10 +0100

    add outlets for ui elements

---
+++ Pitch-Perfect-IOS/RecordAudioViewController.swift
@@ -0,0 +1,22 @@
+//
+//  RecordAudioViewController.swift
+//  Pitch-Perfect-IOS
+//
+//  Created by Pedro Del Gallego on 20/03/15.
+//  Copyright (c) 2015 Pedro Del Gallego. All rights reserved.
+//
+
+import UIKit
+
+class RecordAudioViewController: UIViewController {
+
+    @IBOutlet weak var recordingLabel: UILabel!
+    
+    @IBOutlet weak var startRecordingButton: UIButton!
+
+    @IBOutlet weak var stopRecordingButton: UIButton!
+
+    override func viewDidLoad() { super.viewDidLoad() }
+
+}
+
